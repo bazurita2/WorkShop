35d72398d36b4d290282bd6f09655ccec3d08fad
bazurita2 <brayanzurita@hotmail.com>
2018-11-30 14:20:43 -0500

    update a new version lab

--- Partial_2/Lab_17/UnitTesting/test/ec/edu/espe/lab_17/logic/OperationTest.java
+++ Partial_2/Lab_17/UnitTesting/test/ec/edu/espe/lab_17/logic/OperationTest.java
@@ -45,6 +45,7 @@ public class OperationTest {
         Operation instance = new Operation();
         short expResult = 0;
         short result = instance.add(addend1, addend2);
+        System.out.println(expResult+" result");
         assertEquals(expResult, result);
         // TODO review the generated test code and remove the default call to fail.
         
@@ -63,20 +64,37 @@ public class OperationTest {
         operand1 = 2;
         operand2 = -1;
         expectedResolve = 1;
+        actualResolve = operation.add(operand1, operand2);
         System.out.println(operand1+" + "+operand2+" succeeded");
+        System.out.println(expectedResolve+" expected");
+        System.out.println(actualResolve+" result");
         assertEquals(expectedResolve, actualResolve);
     }
     
-     @Test
-    public void wrongCases(){
+    @Test
+    public void testWrongCases(){
         short operand1 = (short) 50000;
         short operand2 = (short) 50000;
         short expectedResolve = 3;
         Operation operation = new Operation();
         short actualResolve = operation.add(operand1, operand2);
         System.out.println(operand1+" + "+operand2+" succeeded");
+        System.out.println(expectedResolve+" expected");
+        System.out.println(actualResolve+" result");
         assertEquals(expectedResolve, actualResolve);
+    }
+    
+    @Test
+    public void testAddCaracter(){
+        short operand1 = '1';
+        short operand2 = '2';
+        short expectedResolve = 3;
+        Operation operation = new Operation();
+        short actualResolve = operation.add((short)operand1,(short)operand2);
         System.out.println(operand1+" + "+operand2+" succeeded");
+        System.out.println(expectedResolve+" expected");
+        System.out.println(actualResolve+" result");
+        assertEquals(expectedResolve, actualResolve);
         
     }
 }
